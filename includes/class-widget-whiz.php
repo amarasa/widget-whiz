34a2ffc6f1ae3436dcf22c48b4459e28ef78e0fe
amarasa <lpangm03@yahoo.com>
2024-11-12 09:31:16 -0500

    Update class-widget-whiz.php

--- includes/class-widget-whiz.php
+++ includes/class-widget-whiz.php
@@ -9,6 +9,7 @@ class Widget_Whiz
     public function __construct()
     {
         add_action('widgets_init', array($this, 'register_dynamic_sidebars'));
+        add_action('get_sidebar', array($this, 'display_selected_sidebar'));
     }
 
     public function register_dynamic_sidebars()
@@ -27,6 +28,19 @@ class Widget_Whiz
                 'before_title' => '<h6 class="side-title">',
                 'after_title' => '</h6>',
             ));
+        }
+    }
+
+    // Display the selected sidebar on the frontend
+    public function display_selected_sidebar()
+    {
+        if (is_singular(array('post', 'page'))) {
+            global $post;
+            $selected_sidebar = get_post_meta($post->ID, '_widget_whiz_selected_sidebar', true);
+
+            if ($selected_sidebar) {
+                dynamic_sidebar(sanitize_title($selected_sidebar));
+            }
         }
     }
 }
